b9828863a58628a832e41ff805f98ad5c178563d
WeakBlood <delfioremarcello@gmail.com>
2025-02-23 12:37:01 +0100

    newFile

---
+++ solved/SORTING & SEARCHING/1660 SUBARRAY SUMS I.cpp
@@ -0,0 +1,50 @@
+/* 
+Sender:	WeakBlood
+Submission time:	2025-02-23 13:31:31 +0200
+Language:	C++ (C++17)
+Status:	READY
+Result:	ACCEPTED
+*/
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define inf 1e9
+using namespace std;
+
+//!!!EXPLANATION!!!:
+/* 
+For this problem we are going to use a map for tracking the frequency in which a certain value appears, 
+and if at any given moment the sum the prefix sum from to i-th value - X points to a value that has already appeared 
+it means that we can form X from that value + our actual pfx, so add that to the answer.
+Time complexity O(N logₙ) 
+*/
+
+//Cases
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef LOCAL
+    ifstream cin("input.txt");
+    #endif
+    int T;
+    T = 1;
+    while(T--){
+        int N;
+        ll X;
+        cin >> N >> X;
+        map<ll,int> freq;
+        ll pfx = 0;
+        freq[pfx]++;
+        ll a;
+        ll ans = 0;
+        for(int i = 0; i < N; i++){
+            cin >> a;
+            pfx+=a;
+            ans+=freq[pfx-X];
+            freq[pfx]++;
+        }   
+        cout << ans << '\n';
+    }
+    return 0;
+}

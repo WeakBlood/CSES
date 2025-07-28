ad2be476b77740e94520d0b5448091628bc72eb9
WeakBlood <delfioremarcello@gmail.com>
2025-07-28 18:43:27 +0200

    newFile

--- solved/INTRO/1068 WEIRD ALGORITHM.cpp
+++ solved/INTRO/1068 WEIRD ALGORITHM.cpp
@@ -1,14 +1,19 @@
-/*Sender:	WeakBlood
-Submission time:	2025-02-10 16:56:38 +0200
-Language:	C++ (C++17)
-Status:	READY
-Result:	ACCEPTED*/
+/* 
+@author WeakBlood<3
+*/
 #include <bits/stdc++.h>
-
+ 
 #define ll long long int
 #define inf 1e9
 using namespace std;
-
+ 
+//!!!EXPLANATION!!!:
+/* 
+ 
+*/
+ 
+//Cases
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -18,10 +23,11 @@ int main(){
     ll N;
     cin >> N;
     cout << N << ' ';
-
     while(N > 1){
-        if(N&1)N = N*3 + 1;
-        else N/=2;
+        if(N&1){
+            N*=3;
+            N++;
+        } else N/=2;
         cout << N << ' ';
     }
     return 0;

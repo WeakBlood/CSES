8211ae72dfbeb230bebea7515e932f8e33759809
WeakBlood <delfioremarcello@gmail.com>
2025-07-28 18:49:22 +0200

    newFile

--- solved/INTRO/1069 REPETITIONS.cpp
+++ solved/INTRO/1069 REPETITIONS.cpp
@@ -1,40 +1,35 @@
-/*
-Sender:	WeakBlood
-Submission time:	2025-02-11 18:03:28 +0200
-Language:	C++ (C++17)
-Status:	READY
-Result:	ACCEPTED
+/* 
+@author WeakBlood<3
 */
 #include <bits/stdc++.h>
-
+ 
 #define ll long long int
 #define inf 1e9
 using namespace std;
-
+ 
+//!!!EXPLANATION!!!:
+/* 
+ 
+*/
+ 
+//Cases
+ 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     #ifdef LOCAL
     ifstream cin("input.txt");
     #endif
-    int T = 1;
-    while(T--){
-        string s;
-        cin >> s;
-        int l = 1;
-        int maxi = 1;
-        char ref = '$';
-        for(char c:s){
-            maxi = max(maxi,l);
-            if(c == ref){
-                l++;
-            } else{
-                l = 1;
-                ref = c;
-            }
-        }
-        maxi = max(maxi,l);
-        cout << maxi << '\n';
+    string s;
+    cin >> s;
+    
+    int N = s.size();
+    int ans = 1;
+    int ref = 0;
+    for(int i = 1; i < N; i++){
+        if(s[ref] != s[i]) ref = i;
+        ans = max(ans,i-ref+1);
     }
+    cout << ans << '\n';
     return 0;
 }

a209f2d44abc05ea0cf0467ec0cc7289a7578a3f
WeakBlood <delfioremarcello@gmail.com>
2025-07-28 19:03:14 +0200

    newFile

--- solved/INTRO/1070 PERMUTATIONS.cpp
+++ solved/INTRO/1070 PERMUTATIONS.cpp
@@ -1,9 +1,5 @@
 /* 
-Sender:	WeakBlood
-Submission time:	2025-02-11 18:44:38 +0200
-Language:	C++ (C++17)
-Status:	READY
-Result:	ACCEPTED
+@author WeakBlood<3
 */
 #include <bits/stdc++.h>
 
@@ -13,12 +9,10 @@ using namespace std;
 
 //!!!EXPLANATION!!!:
 /* 
-This approach works because if we take x which is let's say even, x+1 is gonna be odd,
-this means that every sequence of odd numbers is gonna have at least an absolute difference of 2 between adjiacent numbers, 
-we can just reduce the problem to "OUTPUT ALL EVEN NUMBERS FROM 2 TO N AND ALL ODD NUMBERS FROM 1 TO N"
+
 */
 
-//THE ONLY IMPOSSIBLE CASES ARE N = 2 AND N = 3
+//Cases
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -30,18 +24,13 @@ int main(){
     while(T--){
         int N;
         cin >> N;
-        int Odd = (N&1 ? N:N-1);
-        int Even = (N&1 ? N-1:N);
-        vector<int> odd,even;
-        for(int i = Odd; i >= 1; i-=2) odd.push_back(i);
-        for(int i = Even; i >= 1; i-=2) even.push_back(i);
-        if(abs(odd.back()-Even) <= 1 && N != 1){
+        if(N == 3 || (N == 2)){
             cout << "NO SOLUTION" << '\n';
-        } else{
-            for(int u:odd) cout << u << ' ';
-            for(int u:even) cout << u << ' ';
-            cout << '\n';
+            continue;
         }
+
+        for(int i = 2; i <= (N%2 != 0 ? N-1:N); i+=2) cout << i << ' ';
+        for(int i = 1; i <= (N%2 == 0 ? N-1:N); i+=2) cout << i << ' ';
     }
     return 0;
 }

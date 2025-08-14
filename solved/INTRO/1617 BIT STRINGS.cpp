cdcfe3fc8e00bee58cac33a3ae27a57d06a9ccff
WeakBlood <delfioremarcello@gmail.com>
2025-08-14 11:13:49 +0200

    newFile

--- solved/INTRO/1617 BIT STRINGS.cpp
+++ solved/INTRO/1617 BIT STRINGS.cpp
@@ -1,9 +1,5 @@
 /* 
-Sender:	WeakBlood
-Submission time:	2025-02-11 19:44:09 +0200
-Language:	C++ (C++17)
-Status:	READY
-Result:	ACCEPTED
+@author WeakBlood<3
 */
 #include <bits/stdc++.h>
 
@@ -13,28 +9,21 @@ using namespace std;
 
 //!!!EXPLANATION!!!:
 /* 
-Let's look at the problem statement by another angle, let's ask ourselves: "How many numbers can we make with N bits?".
-Now, we all know that with N bits we can make 2ᴺ different numbers ( 0 included ), but there is a problem here,
-2ᴺ with N = 10⁶ could be pretty big, so the problem asks to do it MODULO 10⁹+7 to make it feasible.
-*/
-
-/*For the solution we will be using a classic but really powerful technique called Binary Exponentiaion 
-that allows us to calculate any pow operation in only O(log₂(N)) time while also specifying a mod.
-// More about this technique on https://cp-algorithms.com/algebra/binary-exp.html
 
 */
 
+//Cases
 
-ll fastExp(ll a, ll exp, ll mod){
-    ll res = 1;
-    while(exp > 0){
-        if(exp&1){
-            res = (res*a)%mod;
+ll binExp(ll a, ll b, ll mod){
+    ll ans = 1;
+    while(b > 0){
+        if(b&1){
+            ans=(ans*a)%mod;
         }
-        a = (a*a)%mod;
-        exp>>=1;
+        a=(a*a)%mod;
+        b>>=1;
     }
-    return res;
+    return ans;
 }
 int main(){
     ios_base::sync_with_stdio(false);
@@ -42,13 +31,9 @@ int main(){
     #ifdef LOCAL
     ifstream cin("input.txt");
     #endif
-    int T;
-    T = 1;
-    while(T--){
-        ll N;
-        cin >> N;
-        ll MOD = 1e9+7;
-        cout << fastExp(2,N,MOD) << '\n';
-    }
-    return 0;
+    ll N;
+    cin >> N;
+    ll mod = 1e9+7;
+    cout << binExp(2LL,N,mod) << '\n';
+    return 0;   
 }

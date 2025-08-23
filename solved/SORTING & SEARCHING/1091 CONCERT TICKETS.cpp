d260709dda2193e07ce1e32552e89bdb2e4418db
WeakBlood <delfioremarcello@gmail.com>
2025-08-23 20:02:25 +0200

    newFile

--- solved/SORTING & SEARCHING/1091 CONCERT TICKETS.cpp
+++ solved/SORTING & SEARCHING/1091 CONCERT TICKETS.cpp
@@ -1,9 +1,5 @@
 /* 
-Sender:	WeakBlood
-Submission time:	2025-02-16 11:57:59 +0200
-Language:	C++ (C++17)
-Status:	READY
-Result:	ACCEPTED
+@author WeakBlood<3
 */
 #include <bits/stdc++.h>
 
@@ -13,11 +9,7 @@ using namespace std;
 
 //!!!EXPLANATION!!!:
 /* 
-For this problem we will need a multiset for storing all the tickets and the upper_bound function which returns the first the first element strictly greater than a certain
-target x, insertions, deletions and upper_bound on multiset are O(logₙ) so each time we have a new customer we search for the first ticket that exceeds their maximum badget,
-if the first one already exceedes it, it means that there are NO solutions and all tickets are above the customer badget, in all other cases, we can go one ticket behind
-and give that to them since it will be the closest to X.
-Time complexity O(N logₙ)
+
 */
 
 //Cases
@@ -28,30 +20,29 @@ int main(){
     #ifdef LOCAL
     ifstream cin("input.txt");
     #endif
-    int T;
-    T = 1;
-    while(T--){
-        int N,Q;
-        cin >> N  >> Q;
+    int N,M;
+    cin >> N >> M;
+    multiset<int> tickets;
+    for(int i = 0; i < N; i++){
+        int t;
+        cin >> t;
+        tickets.insert(t);
+    }
 
-        multiset<int> tickets;
-        while(N--){
-            int t;
-            cin >> t;
-            tickets.insert(t);
-        }
-
-        while(Q--){
-            int X;
-            cin >> X;
-            auto it = tickets.upper_bound(X);
-            if(tickets.begin() == it){
-                cout << -1 << '\n';
-            } else{
+    for(int i = 0; i < M; i++){
+        int customer;
+        cin >> customer;
+        if(!tickets.empty()){
+            auto it = tickets.upper_bound(customer);
+            if(it!=tickets.begin()){
                 it--;
-                cout << *it << '\n';
+                cout << *(it) << '\n';
                 tickets.erase(it);
+            } else{
+                cout << -1 << '\n';
             }
+        } else{
+            cout << -1 << '\n';
         }
     }
     return 0;

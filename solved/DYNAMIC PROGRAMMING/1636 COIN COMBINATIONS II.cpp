b7cb4c93fc1731cf1c1c691830082fe64157c813
WeakBlood <123515315+WeakBlood@users.noreply.github.com>
2025-10-25 21:21:36 +0200

    Add files via upload

---
+++ solved/DYNAMIC PROGRAMMING/1636 COIN COMBINATIONS II.cpp
@@ -0,0 +1,40 @@
+/* 
+@author WeakBlood<3
+*/
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define inf 1e9
+const int mod = 1e9+7;
+using namespace std;
+
+//!!!EXPLANATION!!!:
+/* 
+
+*/
+
+//Cases
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef LOCAL
+    ifstream cin("input.txt");
+    #endif
+    int N,X;
+    cin >> N >> X;
+    vector<int> coins(N);
+    for(int i = 0; i < N; i++){
+        cin >> coins[i];
+    }
+
+    vector<int> dp(X+1,0);
+    dp[0]=1;
+    for(int coin:coins){
+        for(int i = 1; i <= X; i++){
+            if(i-coin>=0) dp[i]=(dp[i]+dp[i-coin])%mod;
+        }
+    }
+    cout << dp[X] << '\n';
+    return 0;
+}
